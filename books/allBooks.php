6e3cd231066d9a24ed67a5b872f39eb292aa1b0c
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2018-10-15 20:21:26 +0000

    all books in db rendered

--- books/allBooks.php
+++ books/allBooks.php
@@ -1,5 +1,13 @@
 <?php
     require '../templates/header.php';
+
+    $sql = "SELECT `id`, `book_name`, `image_name` FROM `books` WHERE 1";
+    $result = mysqli_query($dbc, $sql);
+    if ($result) {
+        $allBooks = mysqli_fetch_all($result, MYSQLI_ASSOC); //what do you want to fetch? The result. And what language do you want this to be?
+    } else {
+        die("ERROR! Something went wrong, cannot get the books");
+    }
  ?>
  <div class="container">
      <div class="row mb-2">
@@ -15,21 +23,29 @@
      </div>
 
      <div class="row">
-        <div class="col-md-4">
-            <div class="card mb-4 shadow-sm">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-                <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                            <a href="book.php" class="btn btn-sm btn-outline-info">View</a>
-                            <a href="update.php" class="btn btn-sm btn-outline-secondary">Edit</a>
+        <?php if($allBooks): ?>
+            <?php foreach($allBooks as $singleBook): ?>
+                <div class="col-md-4">
+                    <div class="card mb-4 shadow-sm">
+                        <img class="card-img-top" src="./books/images/uploads/thumbnails/<?= $singleBook['image_name'] ?>" alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text"><?= $singleBook['book_name'] ?></p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <a href="book.php" class="btn btn-sm btn-outline-info">View</a>
+                                    <a href="update.php" class="btn btn-sm btn-outline-secondary">Edit</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+            <?php endforeach; ?>
+        <?php else: ?>
+            <div class="col">
+                <p>Sorry, there aren't any books in the library at the moment.</p>
             </div>
-        </div>
-    </div>
+        <?php endif; ?>
+
 
 
  </div>
